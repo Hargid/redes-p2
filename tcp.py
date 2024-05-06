02f8217dc060b24ce7dc74b2fac2a442928ea3d3
Hargid <kingharu@gmail.com>
2024-05-06 06:52:32 -0300

    Passo 1

--- tcp.py
+++ tcp.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from tcputils import *
 
@@ -32,11 +33,27 @@ class Servidor:
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
+            #Numero aleatorio para sequência de segmentos do servidor:
+            seqNoAns = int.from_bytes(os.urandom(4), byteorder='big')
+            
+            #Proximo segmento esperado
+            ack_no = seq_no + 1
+            
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+            
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
+            
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
+            #Handshake -> enviando ID da conexao para o cliente
+            handshake = make_header(dst_port, src_port, seqNoAns, ack_no, FLAGS_SYN|FLAGS_ACK)
+            handshake = fix_checksum(handshake, dst_addr, src_addr)
+            
+            self.rede.enviar(handshake, src_addr)
+            
+            
+            
             if self.callback:
                 self.callback(conexao)
         elif id_conexao in self.conexoes:
@@ -48,9 +65,11 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seqNoAns):
         self.servidor = servidor
         self.id_conexao = id_conexao
+        #Numero de Sequencia da Conexao
+        self.seqNoAns = seqNoAns
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
